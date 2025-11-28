32674f5bb543d547d5562fe45146de7db2b912aa
Kasiwarrior <isakwfarje@outlook.com>
2025-11-28 14:31:46 +0100

    Fixed stuff

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 const styleCheckbox = document.getElementById("divStyle");
 
 const textfields = document.getElementsByClassName("textfield");
@@ -9,47 +6,28 @@ const removeButton = document.querySelector(".remove-button");
 
 const emptyDiv = document.querySelector(".empty-div");
 
-
-
-
 function handleInputEvent(e) {
-   
-    console.log("Event från:", e.target, e.target.value);
+  console.log("Event från:", e.target, e.target.value);
 
-   
-    const fieldName = e.target.name;
+  const fieldName = e.target.name;
 
-
-    if (fieldName === "content") {
-        emptyDiv.innerHTML = e.target.value;
-    }
+  if (fieldName === "content") {
+    emptyDiv.innerHTML = e.target.value;
+  }
 }
-
 
 const colorInput = document.querySelector('input[name="color"]');
 
-
 styleCheckbox.addEventListener("change", function () {
-    const chosenColor = colorInput.value;
+  const chosenColor = colorInput.value;
 
-     
-        emptyDiv.style.backgroundColor = chosenColor;
-    
-   
+  emptyDiv.style.backgroundColor = chosenColor;
 });
 
-
-
 for (let i = 0; i < textfields.length; i++) {
-    textfields[i].addEventListener("input", handleInputEvent);
-    
+  textfields[i].addEventListener("input", handleInputEvent);
 }
 
-
 removeButton.addEventListener("click", function () {
-    
-   
-    emptyDiv.remove();
-   
-    
+  emptyDiv.remove();
 });
