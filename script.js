8b87bf754e92913c5fcc110ff5e5fa976bdf13ed
Pär Dahlström <gimsbarke354@gmail.com>
2025-11-28 12:14:03 +0100

    Påbörjat uppgifter 4 och 5

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+
+const styleCheckbox = document.getElementById("divStyle");
+
+const textfields = document.getElementsByClassName("textfield");
+
+const removeButton = document.querySelector(".remove-button");
+
+const emptyDiv = document.querySelector(".empty-div");
+
+
+
+
+function handleInputEvent(e) {
+   
+    console.log("Event från:", e.target, e.target.value);
+
+   
+    const fieldName = e.target.name;
+    console.log("Name-attribut:", fieldName);
+
+
+    if (fieldName === "content") {
+        emptyDiv.innerHTML = e.target.value;
+    }
+}
+
+
+let colorInput = null;
+for (let i = 0; i < textfields.length; i++) {
+    if (textfields[i].name === "color") {
+        colorInput = textfields[i];
+        break;
+    }
+}
+
+
+styleCheckbox.addEventListener("change", function () {
+    const chosenColor = colorInput.value;
+
+    if (styleCheckbox.checked){
+        emptyDiv.style.backgroundColor = chosenColor;
+    }
+    else{
+        emptyDiv.style.backgroundColor="";
+    }
+});
+
+
+
+for (let i = 0; i < textfields.length; i++) {
+    textfields[i].addEventListener("input", handleInputEvent);
+    
+}
+
+
+removeButton.addEventListener("click", function () {
+    
+   
+    emptyDiv.remove();
+    //emptyDiv.innerHTML = "";
+    
+});
